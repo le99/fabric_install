6c6005dc2f1c9bfc4a99f286e22a0634778ba48a
le99 <le.florez602@uniandes.edu.co>
2023-02-15 16:15:25 -0500

    Update install-fabric2.2.sh

--- fabric2_2/install-fabric2.2.sh
+++ fabric2_2/install-fabric2.2.sh
@@ -66,9 +66,10 @@ install_docker(){
 	docker --version
 	docker compose version
 
-
-	curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
+	#https://forums.docker.com/t/command-not-found-when-i-try-to-run-docker-compose/97183/3
+	apt-get install docker-compose
+	#curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+	#chmod +x /usr/local/bin/docker-compose
 }
 
 install_go(){
